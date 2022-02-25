3b61d493c906e99d4d712d134d080632b431ccf4
Pedro Henrique da Silva Melo <88786258+PedroHSM26@users.noreply.github.com>
2022-02-25 12:02:59 -0300

    Add files via upload

---
+++ Trabalho Prático - Aluguel de Bicicletas Compartilhadas2/src/teste/Usuario.java
@@ -0,0 +1,23 @@
+package teste;
+
+import java.util.Date;
+
+public class Usuario {
+	
+	private String email;
+	private String senha;
+	private  String nomeCompleto;
+	private String nacionalidade;
+	private Date dataNascimento ;
+	private Long idUsuario ;
+	private int cpf;
+	private PlanoAssinatura tipoDePlano;
+	private Bicicleta bicicleta;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
